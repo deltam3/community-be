d03755ac7749689e7d24dbcccf68c7ea5c752ca9
deltam3 <practicect@proton.me>
2024-10-11 16:32:41 +0900

    cleanup

--- community-be/src/app.controller.ts
+++ community-be/src/app.controller.ts
@@ -1,32 +1,3 @@
-// // import { Controller, Get } from '@nestjs/common';
-// // import { AppService } from './app.service';
-
-// // @Controller()
-// // export class AppController {
-// //   constructor(private readonly appService: AppService) {}
-
-// //   @Get()
-// //   getHello(): string {
-// //     return this.appService.getHello();
-// //   }
-// // }
-
-// import { Controller, Get, Ip } from '@nestjs/common';
-// import { AppService } from './app.service';
-// import { WinstonLogger } from '../config/winston.logger';
-
-// @Controller()
-// export class AppController {
-//   private readonly winstonlogger = new WinstonLogger();
-//   constructor(private readonly appService: AppService) {}
-
-//   @Get()
-//   getHello(@Ip() Ip: string): string {
-//     this.winstonlogger.info(`GetHello call from Ip: ${Ip}`);
-//     return this.appService.getHello();
-//   }
-// }
-
 import { Controller, Get, Ip } from '@nestjs/common';
 import { AppService } from './app.service';
 import { WinstonLogger } from '../config/winston.logger';
